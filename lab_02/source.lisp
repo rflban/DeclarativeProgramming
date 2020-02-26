29538953ca0a36117433f745ce0feffa2ee4dba4
rflban <nabiev.alsn@yandex.ru>
2020-02-27 00:10:35 +0300

    fix reports 1 2

--- lab_02/source.lisp
+++ lab_02/source.lisp
@@ -11,13 +11,13 @@
 (caddr '((abc) (def) (ghi))) ; (ghi)
 
 ;;; 2)
-(list 'Fred 'and Wilma) ; error
+(list 'Fred 'and 'Wilma) ; (Fred and Wilma)
 (list 'Fred '(and Wilma)) ; (Fred (and Wilma))
 (cons nil nil) ; (nil)
 (cons t nil) ; (t)
 (cons nil t) ; (nil . t)
 (list nil) ; (nil)
-(cons (t) nil) ; error
+(cons '(t) nil) ; ((t))
 (list '(one two) '(free temp)) ; ((one two) (free temp))
 (cons 'Fred '(and Wilma)) ; (Fred and Wilma)
 (cons 'Fred '(Wilma)) ; (Fred Wilma)
@@ -25,7 +25,7 @@
 (list t nil) ; (t nil)
 (list nil t) ; (nil t)
 (cons t (list nil)) ; (t nil)
-(list (t) nil) ; error
+(list '(t) nil) ; ((t) nil)
 (cons '(one two) '(free temp)) ; ((one two) free temp)
 
 ;;; 3)
@@ -39,5 +39,17 @@
 
 (defun f3 (ar1)
   (list (list (list ar1))))
+(f3 'ar1)
+
+(defun f1 (ar1 ar2 ar3 ar4)
+  (cons (cons ar1 (cons ar2 nil)) (cons (cons ar3 (cons ar4 nil)) nil)))
+(f1 'ar1 'ar2 'ar3 'ar4)
+
+(defun f2 (ar1 ar2)
+  (cons (cons ar1 nil) (cons (cons ar2 nil) nil)))
+(f2 'ar1 'ar2)
+
+(defun f3 (ar1)
+  (cons (cons (cons ar1 nil) nil) nil))
 (f3 'ar1)
 
