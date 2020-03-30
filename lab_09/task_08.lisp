17228445d1a8da79dd3c625ed19d949c80fcae4e
rflban <nabiev.alsn@yandex.ru>
2020-03-30 15:25:16 +0300

    add testing for task 8

--- lab_09/task_08.lisp
+++ lab_09/task_08.lisp
@@ -1,6 +1,24 @@
-;;; через функционалы
+(reduce #'+ nil)
+(reduce #'* nil)
+(reduce #'+ '(2))
+(reduce #'* '(2))
+(reduce #'* '(word))
 
-;;; рекурсивно
+(+)
+(*)
 
-;;; тестирование
+(reduce #'list nil)
+(reduce #'list '(2))
+(reduce #'list '(2 3))
+
+(list)
+
+(reduce #'cons nil)
+(reduce #'cons '(2))
+(reduce #'cons '(2 3))
+
+(defun foo (arg1 arg2) 1)
+(reduce #'foo nil)
+(reduce #'foo '(2))
+(reduce #'foo '(2 3))
 
